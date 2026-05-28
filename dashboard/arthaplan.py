ad34346d05bc6151e57ca3a41d7b75fff447c076
alifya dinda <23430001.alifya@tsu.ac.id>
2026-05-28 19:09:55 +0700

    arthaplan.py

---
+++ dashboard/arthaplan.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv(
+    'clean_arthaplan.csv'
+)
+
+st.title(
+    'ArthaPlan Dashboard'
+)
+
+# total pengeluaran
+st.metric(
+    'Total Pengeluaran',
+    f"Rp {df['amount_rupiah'].sum():,.0f}"
+)
+
+# fraud
+st.subheader(
+    'Fraud Transaction'
+)
+
+st.bar_chart(
+    df['is_fraud']
+    .value_counts()
+)
+
+# kategori user
+st.subheader(
+    'Kategori Pengguna'
+)
+
+st.bar_chart(
+    df['user_category']
+    .value_counts()
+)
+
+# warning budget
+st.subheader(
+    'Budget Warning'
+)
+
+warning = df[
+    df['budget_usage'] >= 90
+]
+
+st.dataframe(
+    warning[
+        [
+            'client_id',
+            'budget_usage',
+            'warning'
+        ]
+    ]
+)
