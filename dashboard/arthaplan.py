cfa85d5a60bb6fe019bc93f9ed8a881fa0a4d84f
alifya dinda <23430001.alifya@tsu.ac.id>
2026-05-30 15:32:13 +0700

    arthaplan.py

--- dashboard/arthaplan.py
+++ dashboard/arthaplan.py
@@ -18,7 +18,7 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("dashboard/main_data(4).csv")
+    return pd.read_csv("dashboard/main_data (4).csv")
 
 df = load_data()
 
